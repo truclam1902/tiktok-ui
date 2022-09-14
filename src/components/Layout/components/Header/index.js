af88c1513900ee37d4464b2e30796cb4fccb3d54
truclam1902 <nhtlama17051@cusc.ctu.edu.vn>
2022-09-14 21:54:20 +0700

    init commit

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+// cx giúp sử dụng tạo class có -
 function Header() {
-    return <h1>Header Page</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
