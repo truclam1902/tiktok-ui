c091c3a49638f78298e5f85e27c09f7d387c6541
truclam1902 <nhtlama17051@cusc.ctu.edu.vn>
2022-09-14 17:42:54 +0700

    init commit

---
+++ src/components/Layout/DefaultLayout/Content/index.js
@@ -0,0 +1,5 @@
+function Content() {
+    return <h1>Content Page</h1>;
+}
+
+export default Content;
---
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -0,0 +1,5 @@
+function Sidebar() {
+    return <h1>Sidebar Page</h1>;
+}
+
+export default Sidebar;
