b04f99119f5bcb739bd0f8ebb38959961682e1e6
truclam1902 <nhtlama17051@cusc.ctu.edu.vn>
2022-09-15 10:21:47 +0700

    init commit

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 // cx giúp sử dụng tạo class có -
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="TikTok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellcheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
