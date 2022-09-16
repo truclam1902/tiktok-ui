60e77bccb8683d7a5bc91297bd6a7c6ca1bc07c9
truclam1902 <nhtlama17051@cusc.ctu.edu.vn>
2022-09-16 12:14:40 +0700

    init commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,68 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    disabled = false,
+    rounded = false,
+    small = false,
+    large = false,
+    className,
+    leftIcon,
+    rightIcon,
+    children,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        small,
+        large,
+        text,
+        disabled,
+        rounded,
+    });
+    // add thêm primary vào button nếu có thuộc tính primary
+
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove events listener when btn is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof key === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faSearch, faSignIn } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import Button from '~/components/Button';
 import Tippy from '@tippyjs/react/headless';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -51,7 +52,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
